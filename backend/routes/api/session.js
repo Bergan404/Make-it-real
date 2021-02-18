c11f81ee398081b7a6d7655db2ab8ce800cfc7e7
Bergan Oudshoorn <Bergan00@gmail.com>
2021-02-18 15:48:06 -0500

    Phase 4 done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,24 +10,47 @@ const router = express.Router();
 router.post(
     '/',
     asyncHandler(async (req, res, next) => {
-      const { credential, password } = req.body;
+        const { credential, password } = req.body;
 
-      const user = await User.login({ credential, password });
+        const user = await User.login({ credential, password });
 
-      if (!user) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = ['The provided credentials were invalid.'];
-        return next(err);
-      }
+        if (!user) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            return next(err);
+        }
 
-      await setTokenCookie(res, user);
+        await setTokenCookie(res, user);
 
-      return res.json({
-        user,
-      });
+        return res.json({
+            user,
+        });
     }),
-  );
+);
+
+// Log out
+router.delete(
+    '/',
+    (req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+);
 
 module.exports = router;
