c233e769d4b6ce40338e5b6f01139cc787d43b3d
Bergan Oudshoorn <Bergan00@gmail.com>
2021-02-18 13:52:40 -0500

    Phase 3 done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,8 +6,7 @@ router.use('/api', apiRouter)
 
 router.get('/hello/world', function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    // res.send('Hello World!');
-    res.send('Fuck you spidaman!')
+    res.send('Hello World!');
 });
 
 module.exports = router;
