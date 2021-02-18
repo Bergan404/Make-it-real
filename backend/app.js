1c8e815f3c44217de7115c1fc715e941eb1dced7
Bergan Oudshoorn <Bergan00@gmail.com>
2021-02-18 12:40:59 -0500

    fixing errors

--- backend/app.js
+++ backend/app.js
@@ -16,35 +16,6 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
-// Catch unhandled requests and forward to error handler
-app.use((_req, _res, next) => {
-    const err = new Error("The requested resource couldn't be found.")
-    err.title = "Resource Not Found";
-    err.errors = ["The requested resource couldn't be found."];
-    err.status = 404;
-    next(err);
-})
-// Process sequelize errors
-app.use((err, _req, _res, next) => {
-    // check if error is a Sequelize error:
-    if (err instanceof ValidationError) {
-      err.errors = err.errors.map((e) => e.message);
-      err.title = 'Validation error';
-    }
-    next(err);
-  });
-  // Error formatter
-app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    console.error(err);
-    res.json({
-      title: err.title || 'Server Error',
-      message: err.message,
-      errors: err.errors,
-      stack: isProduction ? null : err.stack,
-    });
-  });
-
 //Security Middleware
 if (!isProduction) {
     // enable cors only in development
@@ -66,5 +37,34 @@ app.use(
     })
 )
 app.use(routes) // Connects all the routes
+
+// Catch unhandled requests and forward to error handler
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.")
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+})
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,8 @@ router.use('/api', apiRouter)
 
 router.get('/hello/world', function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
+    // res.send('Hello World!');
+    res.send('Fuck you spidaman!')
 });
 
 module.exports = router;
