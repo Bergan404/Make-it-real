fa3a9efb29eeaa6bb0d38a138d8d8d17e81e0227
Bergan Oudshoorn <Bergan00@gmail.com>
2021-02-19 11:36:36 -0500

    SetUP phase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,19 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import App from './App';
 
 import configureStore from './store';
 
 const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
@@ -22,7 +30,7 @@ function Root() {
         <App />
       </BrowserRouter>
     </Provider>
-  )
+  );
 }
 
 ReactDOM.render(
