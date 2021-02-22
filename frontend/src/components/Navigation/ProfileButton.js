bfcc4861b489a981f0e2c3b2d2c5a1f3038580e6
Bergan Oudshoorn <Bergan00@gmail.com>
2021-02-22 18:23:35 -0500

    navbar styling

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import {NavLink} from 'react-router-dom'
 
@@ -6,26 +5,8 @@ import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
   const {username} = useSelector(state => state.session.user)
-  console.log(username)
-
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
-
-  useEffect(() => {
-    if (!showMenu) return;
-
-    const closeMenu = () => {
-      setShowMenu(false);
-    };
-
-    document.addEventListener('click', closeMenu);
-
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+  // console.log(username)
 
   const logout = (e) => {
     e.preventDefault();
@@ -39,15 +20,8 @@ function ProfileButton({ user }) {
         <i className="fas fa-user-circle" />
       </button>
     </NavLink>
-      {/* {showMenu && (
-        <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
-      )} */}
+      <button className="logout-button" onClick={logout}>Log Out</button>
+      <NavLink className="create-post" to="/">Create Post</NavLink>
     </>
   );
 }
